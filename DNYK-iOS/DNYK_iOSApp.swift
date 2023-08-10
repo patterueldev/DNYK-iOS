3f09dc7ba8da5264861f770c13d94223fca0c755
John Patrick Teruel <jpteruel95@gmail.com>
2023-08-10 09:49:06 +0800

    feat: Initialized iOS Project

---
+++ DNYK-iOS/DNYK_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  DNYK_iOSApp.swift
+//  DNYK-iOS
+//
+//  Created by John Patrick Teruel on 8/10/23.
+//
+
+import SwiftUI
+
+@main
+struct DNYK_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
