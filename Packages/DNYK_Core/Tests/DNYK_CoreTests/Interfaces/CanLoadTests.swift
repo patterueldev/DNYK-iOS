d25ffba5d708eb86924524caee748dfac5e1a7ce
John Patrick Teruel <jpteruel95@gmail.com>
2023-08-21 13:33:56 +0800

    feat: Created unit test for CanLoad protocol

---
+++ Packages/DNYK_Core/Tests/DNYK_CoreTests/Interfaces/CanLoadTests.swift
@@ -0,0 +1,32 @@
+import XCTest
+
+@testable import DNYK_Core
+
+class CanLoadTests: XCTestCase {
+    var canLoad: (any CanLoad)!
+    
+    class MockCanLoad: CanLoad {
+        var isLoading: Bool = false
+    }
+    
+    override func setUp() {
+        super.setUp()
+        canLoad = MockCanLoad()
+    }
+    
+    func testToggleLoadingWithoutParameters() async {
+        await canLoad.toggleLoading()
+        XCTAssertTrue(canLoad.isLoading)
+        
+        await canLoad.toggleLoading()
+        XCTAssertFalse(canLoad.isLoading)
+    }
+    
+    func testToggleLoadingWithParameters() async {
+        await canLoad.toggleLoading(true)
+        XCTAssertTrue(canLoad.isLoading)
+        
+        await canLoad.toggleLoading(false)
+        XCTAssertFalse(canLoad.isLoading)
+    }
+}
